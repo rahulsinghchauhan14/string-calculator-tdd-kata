65945dabee50edc87fe880b668bd38c65cbba3c3
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-06-25 23:44:08 +0530

    write test case for check if string contain negative number

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -6,8 +6,12 @@ describe("add module", () => {
         expect(add("")).toBe(0);
     });
 
-    test('numbers of array is empty', () => {
+    test('numbers of array is empty should return 0', () => {
         expect(add("//;\n")).toBe(0);
+    });
+
+    test('string contain negative number should throw an error', () => {
+        expect(() => add('\n-4')).toThrow(Error);
     });
 
     
