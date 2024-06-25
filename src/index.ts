ab6ee7f4b1f5afbd278c151462cdab0e6c8eab57
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-06-25 23:38:18 +0530

    create fun for get numbers of array from string and write test for check if array is empty

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
 export function add(str: string): number{
-    return 0;
+    
+    // return 0 if input string is empty
+    if(str==="") return 0;
+    
+    // get array which contain numbers
+    const numbersArray: Array<number> = getNumbersFromString(str);
+ 
+    // check if number of array is empty or not
+    if(numbersArray.length == 0){
+        return 0;
+    }
+
+    return 1;
+ 
 }
+
+
+function getNumbersFromString(str: string): Array<number>{
+    let delimiters = ['//',';','\n'];
+    let stringArray: Array<string>;
+    delimiters.forEach(delimiter => {
+        str = str.split(delimiter).join(',');
+    });
+    stringArray = str.split(",");
+    return stringArray.filter(value => value !== '').map(no => parseInt(no));
+}
+
--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -1,7 +1,14 @@
 const { add } = require('./index');
 
 describe("add module", () => {
-    test(' string is empty should return 0', ()=>{
+    
+    test('string is empty should return 0', ()=>{
         expect(add("")).toBe(0);
-    })
+    });
+
+    test('numbers of array is empty', () => {
+        expect(add("//;\n")).toBe(0);
+    });
+
+    
 });
