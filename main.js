1b14bb97a1d46d718342daf65c8a47df7d97806f
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-06-26 01:40:45 +0530

    push final code

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const { add } = require('./dist/index');
 const inputArr = ['','//;\n1,3;4','2','1,4,3,5', '4\n5',0,'[2,3,4]'];
 
 inputArr.forEach(input => {
-    console.log(add(input));
+    console.log(`input: ${input} => `, add(input) , '\n' );
 });
